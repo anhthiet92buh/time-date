4b678c181475f9ea8af95f5df1388520cd1b2894
Thiet <anhthiet92buh@gmail.com>
2020-12-14 23:13:13 +0700

    main

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int x;
+    do
+    {
+        cin>>x;
+        cout<<"---- x = "<<x<<endl;
+    } while (1);
+    
+    cin.get();
+    return 0;
+}
